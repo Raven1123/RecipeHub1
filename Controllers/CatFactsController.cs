f59b45431a77aa0b2f116e4958f235b7a20fe493
Raven1123 <164591968+Raven1123@users.noreply.github.com>
2024-10-19 22:35:54 +0800

    Add files via upload

---
+++ Controllers/CatFactsController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Recipehub.Models;
+namespace Recipehub.Controllers
+{
+    public class CatFactsController : Controller
+    {
+        HttpClient client = new HttpClient();
+        // GET: CatController1
+        public async Task<IActionResult> Index()
+        {
+            string url = "https://catfact.ninja/fact";
+            var response = await client.GetStringAsync(url);
+            var catFact = JsonConvert.DeserializeObject<CatFact>(response);
+
+            return View(catFact);
+        }
+
+        // GET: CatFactsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CatFactsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CatFactsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CatFactsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CatFactsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CatFactsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CatFactsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Controllers/FoodController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Recipehub.Models;
+using System.Text;
+
+namespace Recipehub.Controllers
+{
+    public class FoodController : Controller
+    {
+        // GET: FoodController
+        public async Task<ActionResult> Index()
+        {
+            string apiUrl = "https://localhost:7247/api/user";
+
+            List<RecipeModel> users = new List<RecipeModel>();
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                var result = await response.Content.ReadAsStringAsync();
+                users = JsonConvert.DeserializeObject<List<RecipeModel>>(result);
+            }
+
+            return View(users);
+        }
+        // GET: FoodController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: FoodController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FoodController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(RecipeModel user)
+        {
+            string apiUrl = "https://localhost:7247/api/user";
+            using (HttpClient client = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(user), Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PostAsync(apiUrl, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(user);
+        }
+
+
+        // GET: FoodController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: FoodController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FoodController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: FoodController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
