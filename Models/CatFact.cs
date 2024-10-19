b6b25afe28ef7945f6d3cfb4b805b7abde559b3e
Raven1123 <164591968+Raven1123@users.noreply.github.com>
2024-10-19 22:35:26 +0800

    Add files via upload

---
+++ Models/CatFact.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace Recipehub.Models
+{
+    public class CatFact
+    {
+        public string Fact { get; set; }
+
+        [JsonProperty("length")]
+        public int Length { get; set; }
+    }
+}
---
+++ Models/RecipeModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Recipehub.Models
+{
+    public class RecipeModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
